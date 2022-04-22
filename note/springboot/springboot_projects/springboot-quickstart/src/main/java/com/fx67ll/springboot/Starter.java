6ac4ec8c4004fcd21d3185084009c0c038fa290a
fx67ll <2717745014@qq.com>
2022-04-22 23:22:43 +0800

    finished project springboot-quickstart

---
+++ note/springboot/springboot_projects/springboot-quickstart/src/main/java/com/fx67ll/springboot/Starter.java
@@ -0,0 +1,24 @@
+package com.fx67ll.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Starter extends SpringBootServletInitializer {
+
+    private static Logger logger = LoggerFactory.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+        logger.info("fx67ll Spring Boot Quick Starting......");
+        SpringApplication.run(Starter.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Starter.class);
+    }
+}
